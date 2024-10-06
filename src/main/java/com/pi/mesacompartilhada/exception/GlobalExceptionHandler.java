a051e3bdbd31d19d0c8cc34e95cd5c4c5712841a
willotavio <willaotavio@gmail.com>
2024-10-06 13:48:41 -0300

    Adicionando mensagem de erro para duplicate key

--- src/main/java/com/pi/mesacompartilhada/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pi/mesacompartilhada/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.pi.mesacompartilhada.exception;
 
+import com.mongodb.ErrorCategory;
 import com.mongodb.MongoWriteException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,7 +33,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DoacaoStateOperationNotSupportedException.class)
     public ResponseEntity<Object> handleDoacaoStateOperationNotSupportedException(DoacaoStateOperationNotSupportedException ex) {
         Map<String, String> errors = new HashMap<>();
-        errors.put("error: ",ex.getMessage());
+        errors.put("error",ex.getMessage());
         return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
     }
 
@@ -41,16 +42,25 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DoacaoStateIllegalArgumentException.class)
     public ResponseEntity<Object> handleDoacaoStateIllegalArgumentException(DoacaoStateIllegalArgumentException ex) {
         Map<String, String> errors = new HashMap<>();
-        errors.put("error: ",ex.getMessage());
+        errors.put("error",ex.getMessage());
         return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
     }
 
-    // Quando um campo
+    // Quando um campo que deve ser unique já existe
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MongoWriteException.class)
     public ResponseEntity<Object> handleMongoWriteException(MongoWriteException ex) {
         Map<String, String> errors = new HashMap<>();
-        errors.put("error: ",ex.getMessage());
+        if(ex.getError().getCategory().equals(ErrorCategory.DUPLICATE_KEY)) {
+            String campo = "";
+            int indexStart = ex.getMessage().indexOf("index: ") + 7;
+            int indexEnd = ex.getMessage().indexOf(" ", indexStart);
+            campo = ex.getMessage().substring(indexStart, indexEnd);
+            errors.put("error", "O campo " + campo + " fornecido já existe");
+        }
+        else {
+            errors.put("error", "Erro ao gravar dados");
+        }
         return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
     }
 
